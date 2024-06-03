b3c4236a00a05e78f8ad1fc358560f85b65b1c01
robinhood6996 <robinypb@yahoo.com>
2024-06-03 17:19:44 +0600

    Platform specific code + dimentions

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
 import {
   Text,
   View,
@@ -7,31 +9,48 @@ import {
   Pressable,
   Modal,
   Alert,
+  SafeAreaView,
+  Platform,
 } from "react-native";
 const logoImage = require("../assets/images/partial-react-logo.png");
 export default function Index() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 10,
-      }}
-    >
-      <ScrollView>
-        <Text>localhost</Text>
-      </ScrollView>
-    </View>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.text}>localhost</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = {
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
   container: {
     flex: 1,
-    flexDirection: "row-reverse",
-    gap: 4,
-    borderWidth: 6,
-    borderColor: "red",
+    paddingTop: Platform.OS === "android" ? 20 : 0,
+  },
+  box: {
+    padding: 20,
+  },
+  text: {
+    ...Platform.select({
+      ios: {
+        color: "red",
+      },
+      android: {
+        color: "white",
+      },
+    }),
   },
 };
